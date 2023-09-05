d54f214a80d19ec9de76d5d56220dc91423a33a9
Jin-hyeok2 <hjhlove1120@gmail.com>
2023-09-06 00:55:42 +0900

    Initial commit

---
+++ boot/boot-meta/src/main/java/com/example/DailyProject.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProject {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyProject.class, args);
+    }
+}
